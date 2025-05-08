eb9da9b8a6d3857cc196fa42bb619698d01f9f37
Cameron Harrison <22010@nayland.school.nz>
2025-05-08 14:59:46 +1200

    Rough outline for stage progression

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('test')
+import tkinter as tk
+
+root = tk.Tk()
+
+class app:
+    def __init__(self , root):
+        self.root = root
+        self.stage = 1
+
+        self.create_window(self.stage)
+
+    def create_window(self , stage):
+
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+
+
+
+root.mainloop()
