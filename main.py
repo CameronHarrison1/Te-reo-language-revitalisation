797b7eb861acfb3a139aa4c3542ea806f06ca34e
Cameron Harrison <22010@nayland.school.nz>
2025-05-09 12:24:04 +1200

    Outline for stage system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-root = tk.Tk()
 
 class app:
     def __init__(self , root):
@@ -14,7 +13,28 @@ class app:
         for widget in self.root.winfo_children():
             widget.destroy()
 
+        if stage == 1:
+            label = tk.Label(self.root , text = "Test")
+            button = tk.Button(self.root , text = "Test" , command = self.advance_stage)
 
+        elif stage == 2:
+            label = tk.Label(self.root , text = "Test_2")
+            button = tk.Button(self.root , text = "Close" , command = root.destroy)
+
+
+        label.pack()
+        button.pack()
+
+
+    def advance_stage(self):
+
+        self.stage += 1
+        self.create_window(self.stage)
+
+
+root = tk.Tk()
+
+app(root)
 
 
 root.mainloop()
