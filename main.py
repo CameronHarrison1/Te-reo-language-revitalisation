b1ca0fa066b7f852d77827fc98f0e4907559c872
Cameron Harrison <22010@nayland.school.nz>
2025-05-13 10:44:08 +1200

    Issues with entrybox not being able to go below other widgets

--- main.py
+++ main.py
@@ -14,16 +14,21 @@ class app:
             widget.destroy()
 
         if stage == 1:
-            label = tk.Label(self.root , text = "Test")
-            button = tk.Button(self.root , text = "Test" , command = self.advance_stage)
+
+            label = tk.Label(self.root , text = "Te reo learning game" , font = ("Arial" , 25))
+            button = tk.Button(self.root , text = "START" , command = self.advance_stage)
 
         elif stage == 2:
+
             label = tk.Label(self.root , text = "Test_2")
             button = tk.Button(self.root , text = "Close" , command = root.destroy)
+            entry = tk.Entry(self.root)
+
+            entry.pack()
 
 
         label.pack()
-        button.pack()
+        button.pack(padx = 10 , pady = 10)
 
 
     def advance_stage(self):
@@ -33,6 +38,8 @@ class app:
 
 
 root = tk.Tk()
+
+root.geometry("400x300+760+390")
 
 app(root)
 
